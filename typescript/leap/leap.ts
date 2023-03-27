d24f3a8165d27eaa639dd2b47d5b05a0ba624f93
melissaAcheng <melcheng12@gmail.com>
2023-03-27 14:18:13 -0400

    leap year problem

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,9 @@
+export function isLeap(year: number): boolean {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 != 0) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
